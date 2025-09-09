8963d567202cb25d1bd4d61ac80e8278074abbd0
Khuzaima-codes <khuzaima.jawanpk@gmai.com>
2025-09-09 06:19:07 -0700

    javascript assignment 2

---
+++ app.js
@@ -0,0 +1,24 @@
+// Chapter 9-11
+
+// Q1
+// var userCity = prompt("enter your city name");
+// if (userCity === "karachi") {
+//   alert("Welcome to city of lights");
+// }
+
+// Q2
+// var userGender = prompt("Enter your gender");
+// if (userGender === "male") {
+//   alert("Good morning sir");
+// } else {
+//   alert("Good morning Ma'am");
+// }
+
+// Q3
+var signalColor = prompt("Enter color of road traffic signal");
+if (signalColor === "red") {
+  alert("Must stop");
+  alert("Must stop");
+} else {
+  
+}
